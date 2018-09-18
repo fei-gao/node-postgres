b6aa24e7e0f733855c9bbc74d5db89417466e5b3
Fei Gao <fgao1@ualberta.ca>
2018-09-18 14:12:51 -0600

    add add_person.js file

---
+++ add_person.js
@@ -0,0 +1,29 @@
+const settings = require("./settings"); // settings.json
+
+const first_name = process.argv[2];
+const last_name = process.argv[3];
+const birthdate = process.argv[4];
+
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        user     : settings.user,
+        password : settings.password,
+        database : settings.database,
+        host     : settings.hostname,
+        port     : settings.port,
+        ssl      : settings.ssl
+    }
+  });
+
+const newPeople = {first_name: `${first_name}`, last_name: `${last_name}`, birthdate: `${birthdate}`};
+// console.log("type----", typeof newPeople);
+  knex('famous_people').insert(newPeople)
+  .asCallback(function(err, result){
+    if (err) {
+        return console.error(err);
+    } else {
+    knex.select("last_name").from("famous_people")
+    .then(console.log);
+    }
+  })
